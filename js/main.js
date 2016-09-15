2cfd6bfd85dda8c378ed87eb9b166e7eee5910ff
NTUMBA <cityzenmboma@yahoo.fr>
2016-09-15 06:06:20 +0200

    Index file: add a title.Portfolio: add 2 comment about random images project so did i into main js file

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// var random_images_array = ["Screen_agence_immo.jpg", "programweb.jpg","/html_skills.png";
+// function getRandomImage(imgAr, path) {
+//     path = path || 'images/'; // default path here
+//     var num = Math.floor( Math.random() * imgAr.length );
+//     var img = imgAr[ 3 ];
+//     var imgStr = '<img src="' + path + img + '" alt = "">';
+//     document.write(imgStr); document.close();
+// }
