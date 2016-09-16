3f4bea64c1abf5c9e709d163ce2fae297e3574eb
NTUMBA <cityzenmboma@yahoo.fr>
2016-09-16 17:10:54 +0200

    Delete pictures from img folder. Substitue images into index file.Fix section'margin.Into portfolio file: add comment for futur random images.Portfolio file: fix text responsivity. Main'js file add comment for a fonction

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 // var random_images_array = ["Screen_agence_immo.jpg", "programweb.jpg","html_skills.png"];
 // function getRandomImage(imgAr, path) {
-//     path = path || "../img/"; // default path here
+//     path = path || "./img/rand"; // default path here
 //     var num = Math.floor( Math.random() * imgAr.length );
 //     var img = imgAr[ num ];
 //     var imgStr = '<img src="' + path + img + '" alt = "">';
-//     document.write(imgStr);
-//     //  document.close();
+//     document.write(imgStr); document.close();
 // }
+// // .image_random
+// getRandomImage(random_images_array);
